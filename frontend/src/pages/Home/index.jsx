d4b9d559b7a9a03da60c2676062ec76027fced31
Stéphane LABROT <stephane.labrot@gmail.com>
2026-01-06 17:22:30 +0100

    page d'accueil OK (+ responsive OK)

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,8 +1,14 @@
 import Banner from "../../components/Banner"
 import bannerHome from "../../assets/Image_Banner_Home.png"
+import Card from "../../components/Card"
 
 let Home_banner_txt = "Chez vous, partout et ailleurs"
 
 export default function Home() {
-  return <Banner title={Home_banner_txt} picture={bannerHome} />
+  return (
+    <div>
+      <Banner title={Home_banner_txt} picture={bannerHome} />
+      <Card />
+    </div>
+  )
 }
