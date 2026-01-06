2934ec083e0e06b5ec785501ea62a65a5e8c9295
Stéphane LABROT <stephane.labrot@gmail.com>
2026-01-06 10:03:20 +0100

    routage + pages (header-footer) créées

---
+++ frontend/src/layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+import "../styles/styles.scss"
+
+export default function MainLayout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  )
+}
---
+++ frontend/src/pages/Fiche_Logement/index.jsx
@@ -0,0 +1,3 @@
+export default function FicheLogement() {
+  return <h1>Bienvenue sur la page Fiche Logement</h1>
+}
